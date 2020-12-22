cb4ff7dc28728197538399c16760998596fc7d85
tt67wq <alex@mys4s.cn>
2020-12-22 20:45:11 +0800

    reply error

--- lib/macros/response.ex
+++ lib/macros/response.ex
@@ -1,11 +1,9 @@
 defmodule Sonnam.Macros.Response do
-
   defmacro __using__(_opts) do
     quote do
       import Sonnam.Macros.Response
     end
   end
-
 
   defmacro reply_succ(conn, data) do
     quote bind_quoted: [conn: conn, data: data] do
@@ -13,10 +11,9 @@ defmodule Sonnam.Macros.Response do
     end
   end
 
-  defmacro reply_err(conn, msg, code \\ 500) do
+  defmacro reply_err(conn, msg \\ "Internal Server Error", code \\ 500) do
     quote bind_quoted: [conn: conn, msg: msg, code: code] do
       json(conn, %{code: code, data: msg})
-      halt(conn)
     end
   end
 end
