f7e519671755dfb23d2537f311b5f9eb0fb50a4e
tt67wq <alex@mys4s.cn>
2020-12-22 15:51:00 +0800

    validator

--- lib/validator.ex
+++ lib/validator.ex
@@ -89,17 +89,17 @@ defmodule Sonnam.Validator do
         end
     end
   end
-
-  defprotocol(Typeable, do: def(typeof(self)))
-  defimpl(Typeable, for: Atom, do: def(typeof(_), do: "Atom"))
-  defimpl(Typeable, for: BitString, do: def(typeof(_), do: "BitString"))
-  defimpl(Typeable, for: Float, do: def(typeof(_), do: "Float"))
-  defimpl(Typeable, for: Function, do: def(typeof(_), do: "Function"))
-  defimpl(Typeable, for: Integer, do: def(typeof(_), do: "Integer"))
-  defimpl(Typeable, for: List, do: def(typeof(_), do: "List"))
-  defimpl(Typeable, for: Map, do: def(typeof(_), do: "Map"))
-  defimpl(Typeable, for: PID, do: def(typeof(_), do: "PID"))
-  defimpl(Typeable, for: Port, do: def(typeof(_), do: "Port"))
-  defimpl(Typeable, for: Reference, do: def(typeof(_), do: "Reference"))
-  defimpl(Typeable, for: Tuple, do: def(typeof(_), do: "Tuple"))
 end
+
+defprotocol(Typeable, do: def(typeof(self)))
+defimpl(Typeable, for: Atom, do: def(typeof(_), do: "Atom"))
+defimpl(Typeable, for: BitString, do: def(typeof(_), do: "BitString"))
+defimpl(Typeable, for: Float, do: def(typeof(_), do: "Float"))
+defimpl(Typeable, for: Function, do: def(typeof(_), do: "Function"))
+defimpl(Typeable, for: Integer, do: def(typeof(_), do: "Integer"))
+defimpl(Typeable, for: List, do: def(typeof(_), do: "List"))
+defimpl(Typeable, for: Map, do: def(typeof(_), do: "Map"))
+defimpl(Typeable, for: PID, do: def(typeof(_), do: "PID"))
+defimpl(Typeable, for: Port, do: def(typeof(_), do: "Port"))
+defimpl(Typeable, for: Reference, do: def(typeof(_), do: "Reference"))
+defimpl(Typeable, for: Tuple, do: def(typeof(_), do: "Tuple"))
