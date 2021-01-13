b8dd841ff7ceb7416605fa84bc4ca3a7644946c3
tt67wq <alex_wan@jiliguala.com>
2021-01-13 10:23:18 +0800

    add aliyun oss tools

---
+++ lib/aliyun/oss.ex
@@ -0,0 +1,51 @@
+defmodule Sonnam.Aliyun.Oss do
+  @moduledoc """
+  阿里云OSS token生成
+  """
+  import Sonnam.Aliyun.OssConfig
+
+  @callback_body """
+  filename=${object}&size=${size}&mimeType=${mimeType}&height=${imageInfo.height}&width=${imageInfo.width}
+  """
+
+  def get_token(upload_dir, expire_sec, callback) do
+    expire =
+      DateTime.now!("Etc/UTC")
+      |> DateTime.add(expire_sec, :second)
+
+    policy =
+      %{
+        "expiration" => DateTime.to_iso8601(expire),
+        "conditions" => [["starts-with", "$key", upload_dir]]
+      }
+      |> Jason.encode!()
+      |> String.trim()
+      |> Base.encode64()
+
+    signature =
+      policy
+      |> Sonnam.Aliyun.Sign.sign(access_key_secret())
+
+
+    base64_callback_body =
+      %{
+        "callbackUrl" => callback,
+        "callbackBody" => @callback_body,
+        "callbackBodyType" => "application/x-www-form-urlencoded"
+      }
+      |> Jason.encode!()
+      |> String.trim()
+      |> Base.encode64()
+
+    %{
+      "accessid" => access_key_id(),
+      "host" => "http://" <> endpoint(),
+      "policy" => policy,
+      "signature" => signature,
+      "expire" => DateTime.to_unix(expire),
+      "dir" => upload_dir,
+      "callback" => base64_callback_body
+    }
+    |> Jason.encode()
+  end
+end
---
+++ lib/aliyun/util.ex
@@ -0,0 +1,24 @@
+defmodule Sonnam.Aliyun.OssConfig do
+  [:endpoint, :access_key_id, :access_key_secret]
+  |> Enum.map(fn config ->
+    def unquote(config)() do
+      :aliyun_oss
+      |> Application.get_env(unquote(config))
+    end
+  end)
+end
+
+defmodule Sonnam.Aliyun.Sign do
+  @moduledoc """
+  签名工具
+  """
+
+  @doc """
+  签名字符串
+  """
+  @spec sign(String.t(), String.t()) :: String.t()
+  def sign(string_to_sign, key) do
+    :crypto.hmac(:sha, key, string_to_sign)
+    |> Base.encode64()
+  end
+end
