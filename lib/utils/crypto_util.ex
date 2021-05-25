401c02fdca52fb837059107a7b656023665140fe
tt67wq <alex_wan@jiliguala.com>
2021-05-25 15:45:17 +0800

    add id generator

--- lib/utils/crypto_util.ex
+++ lib/utils/crypto_util.ex
@@ -20,9 +20,6 @@ defmodule Sonnam.Utils.CryptoUtil do
   @spec sha256(String.t()) :: binary()
   def sha256(plaintext), do: :crypto.hash(:sha256, plaintext)
 
-  @spec hmac_sha256(String.t(), String.t()) :: binary()
-  def hmac_sha256(key, plaintext), do: :crypto.hmac(:sha256, key, plaintext)
-
   @doc """
   generate random string
   ## Example
@@ -39,50 +36,20 @@ defmodule Sonnam.Utils.CryptoUtil do
   end
 
   @doc """
-  aes加密
+  generate id with prefix and tail
+
+  ## Examples
+
+  iex> generate("U", 4)
+  "U16219286427399"
   """
-  @spec aes_encrypt(String.t(), String.t(), list()) :: binary() | String.t()
-  def aes_encrypt(plaintext, key, options \\ [base64: true]) do
-    iv = :crypto.strong_rand_bytes(16)
-    ciphertext = :crypto.block_encrypt(:aes_cbc256, sha256(key), iv, pkcs7_pad(plaintext))
-    res = iv <> ciphertext
+  def generate_id(prefix, tail_len) do
+    tail =
+      1..tail_len
+      |> Enum.map(fn _ -> Enum.random(0..9) end)
+      |> Enum.join()
 
-    if options[:base64], do: Base.encode64(res), else: res
-  end
-
-  # 补全至16字节整数倍
-  defp pkcs7_pad(message) do
-    bytes_remaining = rem(byte_size(message), 16)
-    padding_size = 16 - bytes_remaining
-    message <> :binary.copy(<<padding_size>>, padding_size)
-  end
-
-  @doc """
-  aes解密
-  """
-  @spec aes_decrypt(String.t(), String.t(), list()) :: String.t()
-  def aes_decrypt(ciphertext, key, options \\ [base64: true]) do
-    {iv, target} =
-      if options[:base64] do
-        {:ok, <<iv::binary-16, target::binary>>} = Base.decode64(ciphertext)
-        {iv, target}
-      else
-        <<iv::binary-16, target::binary>> = ciphertext
-        {iv, target}
-      end
-
-    {:ok, plaintext} =
-      :crypto.block_decrypt(:aes_cbc256, sha256(key), iv, target)
-      |> pkcs7_unpad()
-
-    plaintext
-  end
-
-  defp pkcs7_unpad(<<>>), do: :error
-
-  defp pkcs7_unpad(message) do
-    padding_size = :binary.last(message)
-    {:ok, binary_part(message, 0, byte_size(message) - padding_size)}
+    "#{prefix}#{:os.system_time(:seconds)}#{tail}"
   end
 
   #### ssl tools
