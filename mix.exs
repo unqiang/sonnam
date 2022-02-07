372f81b61f1240cc039fd95e8e9dc8402954cb9d
wanqiang <wanqiang@shizhuang-inc.com>
2022-02-07 20:16:46 +0800

    update deps

--- mix.exs
+++ mix.exs
@@ -21,12 +21,12 @@ defmodule Sonnam.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:tzdata, "~> 1.0.5"},
-      {:redix, "0.11.2"},
-      {:httpoison, "~> 1.7"},
-      {:jason, "~> 1.2"},
-      {:poolboy, "~> 1.5.1"},
-      {:strukt, "~> 0.2.1"},
+      {:tzdata, "~> 1.1"},
+      {:redix, "~> 1.1"},
+      {:httpoison, "~> 1.8"},
+      {:jason, "~> 1.3"},
+      {:poolboy, "~> 1.5"},
+      {:strukt, "~> 0.3"},
       {:mime, "~> 2.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
