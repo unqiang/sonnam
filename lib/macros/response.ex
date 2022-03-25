b4a2fd9635f42e8351c10e0a25ac6e95bca2d3a8
wanqiang <wanqiang@shizhuang-inc.com>
2022-03-25 16:35:52 +0800

    upgrade resp macro

--- lib/macros/response.ex
+++ lib/macros/response.ex
@@ -1,18 +1,17 @@
 defmodule Sonnam.Macros.Response do
-  defmacro __using__(_opts) do
-    quote do
-      import Sonnam.Macros.Response
-    end
-  end
+
+  require Logger
 
   defmacro reply_succ(conn, data \\ "success") do
     quote bind_quoted: [conn: conn, data: data] do
+      Logger.info("reply succ => #{inspect(data)}")
       json(conn, %{code: 200, data: data})
     end
   end
 
   defmacro reply_err(conn, msg \\ "Internal server error", code \\ 500) do
     quote bind_quoted: [conn: conn, msg: msg, code: code] do
+      Logger.error("reply err => #{inspect(msg)}")
       json(conn, %{code: code, msg: msg})
     end
   end
