3c29b079caa3089f6ef6f885b6dd7e99e670a056
wq <wq@shizhuang-inc.com>
2022-05-18 17:40:35 +0800

    add doc for cache module

--- lib/macros/cache.ex
+++ lib/macros/cache.ex
@@ -1,5 +1,11 @@
 defmodule Sonnam.Macros.Cache do
-  @moduledoc false
+  @moduledoc """
+  缓存框架
+  store: 实现了resolve方法的存储模块
+  mod: 实现了 set_dirty, get_dirty和get_source的模块
+
+  NOTE: get_source实现 MUST NOT 返回nil结果!!!
+  """
 
   @callback set_dirty(key :: String.t(), flag :: boolean()) :: :ok
   @callback get_dirty(key :: String.t()) :: boolean()
