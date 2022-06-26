0f8c527425e11aa301e3a27d9b07fc5d33480ddf
wq <wq@shizhuang-inc.com>
2022-06-26 23:43:56 +0800

    prune nils when response

--- lib/macros/response.ex
+++ lib/macros/response.ex
@@ -2,11 +2,26 @@ defmodule Sonnam.Macros.Response do
   defmacro __using__(_opts) do
     quote do
       require Logger
+      defguard is_struct(term) when is_map(term) and :erlang.is_map_key(:__struct__, term)
+
+      @spec reducer({any, any}, map) :: map
+      defp reducer({k, v}, map) when is_map(v), do: Map.put(map, k, prune_nils(v))
+      defp reducer({k, v}, map) when is_list(v), do: Map.put(map, k, prune_nils(v))
+      defp reducer({_k, v}, map) when is_nil(v), do: map
+      defp reducer({k, v}, map), do: Map.put(map, k, v)
+
+      @spec prune_nils(term()) :: term()
+      def prune_nils(s) when is_struct(s),
+        do: s |> Map.from_struct() |> Enum.reduce(%{}, &reducer/2)
+
+      def prune_nils(m) when is_map(m), do: Enum.reduce(m, %{}, &reducer/2)
+      def prune_nils(m) when is_list(m), do: Enum.map(m, &prune_nils(&1))
+      def prune_nils(m), do: m
 
       def reply_succ(conn, data \\ "success") do
-        Logger.debug(%{"data" => inspect(data)})
+        Logger.debug(%{"data" => data})
 
-        json(conn, %{code: 200, data: data})
+        json(conn, %{code: 200, data: prune_nils(data)})
       end
 
       def reply_err(conn, msg \\ "Internal server error", code \\ 400)
