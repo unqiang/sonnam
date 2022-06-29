8d0de469c2f775f341c5f29de0ed23c5e585701a
hejinqiang <hejinqiang@aixlu.com>
2022-06-29 15:28:35 +0800

    add recon&observer_cli for trace

--- mix.exs
+++ mix.exs
@@ -27,7 +27,8 @@ defmodule Sonnam.MixProject do
       {:jason, "~> 1.3"},
       {:strukt, "~> 0.3"},
       {:mime, "~> 2.0"},
-      {:nimble_pool, "~> 0.2"}
+      {:nimble_pool, "~> 0.2"},
+      {:observer_cli, "~> 1.7"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
